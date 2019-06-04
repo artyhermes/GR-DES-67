b9e3cfc3b94aac38cea2124d27fc38ee66ae38d2
Ross Meredith <meredith_ross_willia@student.smc.edu>
2019-06-04 09:18:52 -0700

    final

--- Final/main.js
+++ Final/main.js
@@ -1,21 +1,78 @@
-var nav = document.getElementById('nav');
-var navlinks = nav.getElementsByTagName('a');
+$("#addit").click(function(){
+  var productId = $("#productId").val();
+  var productName = $("#productName").val();
+  var productQuantity = $("#productQuantity").val();
+  var data = {
+    'product_id': productId,
+    'product_name': productName,
+    'quantity': productQuantity
+  };
 
-function toggleNav() {
-    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
-  }
+  $.post("/cart/add", data, showDone);
+ });
+ 
+ var showDone = function() {
+  /* Make something happen here*/
+ }
 
-document.getElementById('nav-icon').addEventListener('click', function(e) {
-    e.preventDefault();
-    toggleNav();
-});
+"use strict";
 
-for(var i = 0; i < navlinks.length; i++) {
-    navlinks[i].addEventListener('click', function() {
-      toggleNav();
+// Load plugins
+const browsersync = require("browser-sync").create();
+const del = require("del");
+const gulp = require("gulp");
+const merge = require("merge-stream");
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
   });
+  done();
 }
 
-$("#centralModalSuccess").on('show.bs.modal', function(){
-  alert("Welcome to Green Tapestry Worldwide");
-});
+// BrowserSync reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap
+  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrap, jquery);
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./**/*.css", browserSyncReload);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
+
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+
+// Export tasks
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
